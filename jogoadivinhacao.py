7d49227139d0d727211fc563f7c50e81311762bb
Julia Visoto <128156753+juliavisoto@users.noreply.github.com>
2024-04-15 15:03:05 -0300

    Add files via upload

--- jogoadivinhacao.py
+++ jogoadivinhacao.py
@@ -1,20 +1,36 @@
+import random;
+
 print('*********************************')
 print('Bem vindo, ao JOGO DE ADIVINHAÇÃO')
 print('*********************************')
 
-numeroSecreto = 17
+#Definindo o número secreto
+numeroSecreto = round(random.random()*100)
+#print(numeroSecreto)
+#Definindo o número de tentativas e rodada
+numeroTentativas = 10
+rodada = 1
 
-chuteString = input('Digite um número')
-#chuteString = input('Digite um número')#
+while(rodada <= numeroTentativas):
+    print('Tentativa',rodada, 'de' , numeroTentativas)
 
-print('Você digitou o número', chuteString)
+#Recebendo o chute do jogador
+    chuteString = input('Digite um número entre 1 e 100: ')
+    chute = int(chuteString)
 
-chute = int(chuteString)
+#Declarando as condições
+    if (numeroSecreto == chute):
+        print('Você acertou!')
+        break
+    elif(chute>numeroSecreto):
+        print('Você errou!! O número secreto é um número menor')
+    else:
+        print('Você errou!! O número secreto é um número maior')
 
-if numeroSecreto == chute:
-    print('Você acertou!')
-elif(chute>numeroSecreto):
-   print('voce errou!! o numero secreto é um numero menor')
-else:
-    print('Você errou!! o numero secreto e um numero maior')
+    #numeroTentativas = numeroTentativas - 1
+    rodada = rodada + 1
+
+
+
+#Aula Elif 26.02.24
 
